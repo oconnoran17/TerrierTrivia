6a5443782926dd6d54e369c0d5913ce2e789ef69
sglasher <55998621+sglasher@users.noreply.github.com>
2019-12-10 18:34:32 -0500

    Update app.js
    Redo

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 (function(){
     //Initialize Firebase
-        var config={
+        const config={
             apiKey:"AIzaSyCOp8f7iPBDQfqUolcH97xgDCgBgFVNZWM",
             authDomain:"quizapp-29372.firebaseapp.com",
             databaseURL:"https://quizapp-29372.firebaseio.com",
@@ -9,20 +9,20 @@
         };
     firebase.initializeApp(config);
     
-    var userDataRef = firebase.database().ref("user").orderByKey();
-    userDataRef.once("value").then(function(snapshot) {
-    snapshot.forEach(function(childSnapshot) {
-      var key = childSnapshot.key;
-      var childData = childSnapshot.val();              
+//     var userDataRef = firebase.database().ref("user").orderByKey();
+//     userDataRef.once("value").then(function(snapshot) {
+//     snapshot.forEach(function(childSnapshot) {
+//       var key = childSnapshot.key;
+//       var childData = childSnapshot.val();              
 
-      var name_val = childSnapshot.val().Name;
-      var id_val = childSnapshot.val().AssignedID;
+//       var name_val = childSnapshot.val().Name;
+//       var id_val = childSnapshot.val().AssignedID;
 
-      $("#name").append(name_val);
-      $("#id").append(id_val);
+//       $("#name").append(name_val);
+//       $("#id").append(id_val);
 
-      });
-    });
+//       });
+//     });
     
     
     
