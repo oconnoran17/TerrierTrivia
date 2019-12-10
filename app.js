6fe4998799b1c87b49dfc7923b0d19208194021e
oconnoran17 <55998904+oconnoran17@users.noreply.github.com>
2019-12-10 17:33:24 -0500

    Update app.js
    method 4

--- app.js
+++ app.js
@@ -1,27 +1,52 @@
 (function(){
     //Initialize Firebase
-        const config={
+        var config={
             apiKey:"AIzaSyCOp8f7iPBDQfqUolcH97xgDCgBgFVNZWM",
             authDomain:"quizapp-29372.firebaseapp.com",
             databaseURL:"https://quizapp-29372.firebaseio.com",
             storageBucket:"quizapp-29372.appspot.com"
-
+            //MessagingSender
         };
     firebase.initializeApp(config);
-    var ref = firebase.database().ref();
+    
+    var userDataRef = firebase.database().ref("UserData").orderByKey();
+    userDataRef.once("value").then(function(snapshot) {
+    snapshot.forEach(function(childSnapshot) {
+      var key = childSnapshot.key;
+      var childData = childSnapshot.val();              
 
-    ref.on("value", function(snapshot) {
-       console.log(snapshot.val());
-    }, function (error) {
-       console.log("Error: " + error.code);
+      var name_val = childSnapshot.val().Name;
+      var id_val = childSnapshot.val().AssignedID;
+
+      $("#name").append(name_val);
+      $("#id").append(id_val);
+
+      });
     });
+    
+    
+    
+    
+//     var userDataRef = firebase.database().ref("user").orderByKey();
+//     userDataRef.once("value").then
+//     (function(snapshot)) {
+//      snapshot.forEach(function(childSnapshot){
+//         var key = childSnapshot.key;
+//         var childData = chil  
+//     var ref = firebase.database().ref();
 
-//     //Get Elements
-//     const preObject=document.getElementById('user');
+//     ref.on("value", function(snapshot) {
+//        console.log(snapshot.val());
+//     }, function (error) {
+//        console.log("Error: " + error.code);
+//     });
 
-//     //Create References
-//     const dbRefObject = firebase.database().ref().child('user');
-//     //Syn Changes
-//     dbRefObject.on('value',snap => console.log(snap.val()));
+     //Get Elements
+     const preObject=document.getElementById('user');
+
+     //Create References
+     const dbRefObject = firebase.database().ref().child('user');
+     //Syn Changes
+     dbRefObject.on('value',snap => console.log(snap.val()));
 
 }());
