73454b616550980e5acc894604f34667c5e88f70
oconnoran17 <55998904+oconnoran17@users.noreply.github.com>
2019-12-11 14:24:12 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,51 +1,38 @@
+ function onDatabaseChange(snap) {
+     var list_html = "<ul>";
+     var colon = ": ";
+     var space = " ";
+     var data_obj = snap.val();
+    var my_div = document.getElementById("leaderboard_div");
+    do {
+     for (var entry in data_obj) {
+         if (data_obj[entry].userScore < data_obj[entry + 1].userScore) {
+                let tmp = data_obj[entry];
+                data_obj[entry] = data_obj[entry + 1];
+                data_obj[entry + 1] = tmp;
+                swapped = true;
+          }
+        }
+    } while(swapped);
+    for (var index in data_obj) {
+         list_html += "<li>" + data_obj[index].userName + colon + space + data_obj[index].userScore + "</li>";
+     }
+     list_html += "</ul>";
+     my_div.innerHTML = list_html;
+ }
+    
 //  function onDatabaseChange(snap) {
 //      var list_html = "<ul>";
 //      var colon = ": ";
 //      var space = " ";
 //      var data_obj = snap.val();
 //     var my_div = document.getElementById("leaderboard_div");
-//     do {
 //      for (var entry in data_obj) {
-//          if (data_obj[entry].userScore > data_obj[entry + 1].userScore) {
-//                 let tmp = data_obj[entry];
-//                 data_obj[entry] = data_obj[entry + 1];
-//                 data_obj[entry + 1] = tmp;
-//                 swapped = true;
-//           }
-//         }
-//     } while(swapped)
+//          list_html += "<li>" + data_obj[entry].userName + colon + space + data_obj[entry].userScore + "</li>";
+//      }
 //      list_html += "</ul>";
 //      my_div.innerHTML = list_html;
 //  }
-    
-//     //Sort Elements
-//     do {
-//         swapped = false;
-//         for (var entry in data_obj) {
-//             if (data_obj[entry].userScore > data_obj[entry + 1].userScore) {
-//                 let tmp = data_obj[entry];
-//                 data_obj[entry] = data_obj[entry + 1];
-//                 data_obj[entry + 1] = tmp;
-//                 swapped = true;
-//             }
-//         }
-//     } while (swapped);
-    
-
-
- function onDatabaseChange(snap) {
-     var list_html = "<ul>";
-     var colon = ": ";
-     var space = " ";
-     //var data_obj = snap.val();
-     var data_obj = snap;
-    var my_div = document.getElementById("leaderboard_div");
-     for (var entry in data_obj) {
-         list_html += "<li>" + data_obj[entry].userName + colon + space + data_obj[entry].userScore + "</li>";
-     }
-     list_html += "</ul>";
-     my_div.innerHTML = list_html;
- }
 
 (function(){
     //Initialize Firebase
@@ -98,10 +85,10 @@
      //dbRefObject.orderByChild("userScore").on("child_added", snap => {console.log(snap.val());
      //});
    //Show sorted values on the page: does not work
-     dbRefObject.orderByChild("userScore").on("child_added", snap => {onDatabaseChange(snap);
-     })
+     //dbRefObject.orderByChild("userScore").on("child_added", snap => {onDatabaseChange(snap);
+     //})
    //Show unsorted values on the screen: works
-     //dbRefObject.on('value',snap => onDatabaseChange(snap));
+     dbRefObject.on('value',snap => onDatabaseChange(snap));
      //dbRefObject.on('value',snap => {console.log(snap.val());
      //});
 }());
