b67206b0fb16d8785c169a85672f8b5a5d0b4458
oconnoran17 <55998904+oconnoran17@users.noreply.github.com>
2019-12-11 13:27:43 -0500

    Update app.js

--- app.js
+++ app.js
@@ -97,8 +97,10 @@
      dbRefObject.orderByChild("userScore").on("child_added", snap => {console.log(snap.val());
      });
    //Show sorted values on the page: does not work
-     dbRefObject.orderByChild("userScore").on("child_added", snap => {onDatabaseChange(snap);
-     });
+     //dbRefObject.orderByChild("userScore").on("child_added", snap => {onDatabaseChange(snap);
+     //});
    //Show unsorted values on the screen: works
-     //dbRefObject.on('value',snap => onDatabaseChange(snap));
+     dbRefObject.on('value',snap => onDatabaseChange(snap));
+     dbRefObject.on('value',snap => {console.log(snap.val());
+     });
 }());
