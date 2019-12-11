0e81d53c79dbcc8e17dd8d132d9731ff532fff8b
oconnoran17 <55998904+oconnoran17@users.noreply.github.com>
2019-12-11 00:24:18 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,22 +1,22 @@
- function onDatabaseChange(snap) {
-     var list_html = "<ul>";
-     var colon = ": ";
-     var space = " ";
-     var data_obj = snap.val();
-    var my_div = document.getElementById("leaderboard_div");
-    do {
-     for (var entry in data_obj) {
-         if (data_obj[entry].userScore > data_obj[entry + 1].userScore) {
-                let tmp = data_obj[entry];
-                data_obj[entry] = data_obj[entry + 1];
-                data_obj[entry + 1] = tmp;
-                swapped = true;
-          }
-        }
-    } while(swapped)
-     list_html += "</ul>";
-     my_div.innerHTML = list_html;
- }
+//  function onDatabaseChange(snap) {
+//      var list_html = "<ul>";
+//      var colon = ": ";
+//      var space = " ";
+//      var data_obj = snap.val();
+//     var my_div = document.getElementById("leaderboard_div");
+//     do {
+//      for (var entry in data_obj) {
+//          if (data_obj[entry].userScore > data_obj[entry + 1].userScore) {
+//                 let tmp = data_obj[entry];
+//                 data_obj[entry] = data_obj[entry + 1];
+//                 data_obj[entry + 1] = tmp;
+//                 swapped = true;
+//           }
+//         }
+//     } while(swapped)
+//      list_html += "</ul>";
+//      my_div.innerHTML = list_html;
+//  }
     
 //     //Sort Elements
 //     do {
@@ -31,21 +31,20 @@
 //         }
 //     } while (swapped);
     
- 
 
 
-//  function onDatabaseChange(snap) {
-//      var list_html = "<ul>";
-//      var colon = ": ";
-//      var space = " ";
-//      var data_obj = snap.val();
-//     var my_div = document.getElementById("leaderboard_div");
-//      for (var entry in data_obj) {
-//          list_html += "<li>" + data_obj[entry].userName + colon + space + data_obj[entry].userScore + "</li>";
-//      }
-//      list_html += "</ul>";
-//      my_div.innerHTML = list_html;
-//  }
+ function onDatabaseChange(snap) {
+     var list_html = "<ul>";
+     var colon = ": ";
+     var space = " ";
+     var data_obj = snap.val();
+    var my_div = document.getElementById("leaderboard_div");
+     for (var entry in data_obj) {
+         list_html += "<li>" + data_obj[entry].userName + colon + space + data_obj[entry].userScore + "</li>";
+     }
+     list_html += "</ul>";
+     my_div.innerHTML = list_html;
+ }
 
 (function(){
     //Initialize Firebase
