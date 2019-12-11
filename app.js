160ff30405b3804bb50b8de952c977a74871fbf2
oconnoran17 <55998904+oconnoran17@users.noreply.github.com>
2019-12-11 16:36:02 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,38 +1,38 @@
- function onDatabaseChange(snap) {
-     var list_html = "<ul>";
-     var colon = ": ";
-     var space = " ";
-     var data_obj = snap.val();
-    var my_div = document.getElementById("leaderboard_div");
-    do {
-     for (var entry in data_obj) {
-         if (data_obj[entry].userScore < data_obj[entry + 1].userScore) {
-                let tmp = data_obj[entry];
-                data_obj[entry] = data_obj[entry + 1];
-                data_obj[entry + 1] = tmp;
-                swapped = true;
-          }
-        }
-    } while(swapped);
-    for (var index in data_obj) {
-         list_html += "<li>" + data_obj[index].userName + colon + space + data_obj[index].userScore + "</li>";
-     }
-     list_html += "</ul>";
-     my_div.innerHTML = list_html;
- }
-    
 //  function onDatabaseChange(snap) {
 //      var list_html = "<ul>";
 //      var colon = ": ";
 //      var space = " ";
 //      var data_obj = snap.val();
 //     var my_div = document.getElementById("leaderboard_div");
+//     do {
 //      for (var entry in data_obj) {
-//          list_html += "<li>" + data_obj[entry].userName + colon + space + data_obj[entry].userScore + "</li>";
+//          if (data_obj[entry].userScore < data_obj[entry + 1].userScore) {
+//                 let tmp = data_obj[entry];
+//                 data_obj[entry] = data_obj[entry + 1];
+//                 data_obj[entry + 1] = tmp;
+//                 swapped = true;
+//           }
+//         }
+//     } while(swapped);
+//     for (var index in data_obj) {
+//          list_html += "<li>" + data_obj[index].userName + colon + space + data_obj[index].userScore + "</li>";
 //      }
 //      list_html += "</ul>";
 //      my_div.innerHTML = list_html;
 //  }
+    
+ function onDatabaseChange(snap) {
+     var list_html = "<ul>";
+     var colon = ": ";
+     var space = " ";
+     var data_obj = snap.val();
+    var my_div = document.getElementById("leaderboard_div");
+     for (var entry in data_obj) {
+         list_html += "<li>" + data_obj[entry].userName + colon + space + data_obj[entry].userScore + "</li>";
+     }
+     list_html += "</ul>";
+     my_div.innerHTML = list_html;
+ }
 
 (function(){
     //Initialize Firebase
