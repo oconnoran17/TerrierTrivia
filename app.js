039a1d3628d3a837a06a37f890c7516f9e07a026
oconnoran17 <55998904+oconnoran17@users.noreply.github.com>
2019-12-11 01:49:00 -0500

    Update app.js

--- app.js
+++ app.js
@@ -96,7 +96,7 @@
      //});
      //dbRefObject.orderByChild("userScore").limitToLast(3).on("child_added", snap => {console.log(snap.val());
      //});
-     dbRefObject.orderByChild("userScore").limitToLast(3).on("child_added", snap => {onDatabaseChange(snap);
+     dbRefObject.orderByChild("userScore").on("child_added", snap => {onDatabaseChange(snap);
      });
      
      //dbRefObject.on('value',snap => onDatabaseChange(snap));.v
