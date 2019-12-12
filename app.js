bbb104bed4d67ff1738ddf19dd60e862412f7ed9
oconnoran17 <55998904+oconnoran17@users.noreply.github.com>
2019-12-12 10:46:20 -0500

    Update app.js

--- app.js
+++ app.js
@@ -17,7 +17,7 @@
      do{
         for (i = 0; i < (array.length-1); i++) {
           if(array[i].score < array[i+1].score) {
-                let tmp = data_obj[i];
+                let tmp = array[i];
                 array[i] = array[i+1];
                 array[i+1] = tmp;
                 swapped = true;
