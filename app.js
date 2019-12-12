cbf15fc5a19f573f88fbde2147035c0d943f9b38
sglasher <55998621+sglasher@users.noreply.github.com>
2019-12-12 11:38:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -128,7 +128,7 @@
      //dbRefObject.orderByChild("userScore").on("child_added", snap => {onDatabaseChange(snap);
      //})
    //Show unsorted values on the screen: works
-     dbRefObject.on('value',snap => onDatabaseChange(snap));
+     dbRefObject.on('value',snap => console.log(snap));
  
 //      dbRefObject.once('value').then(function(snapshot){
     
